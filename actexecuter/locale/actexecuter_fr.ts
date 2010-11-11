31af84c24729226bc538b7f5f509b08332d5ae18
Jmgr <jmgr@jmgr.info>
2010-11-11 19:26:41 +0100

    Translation - added missing translations

---
+++ actexecuter/locale/actexecuter_fr.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>CodeExecuter</name>
+    <message>
+        <location filename="../codeexecuter.cpp" line="72"/>
+        <source>Backtrace: </source>
+        <translation>Pile d&apos;appels :</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../codeexecuter.cpp" line="71"/>
+        <source>Uncaught exception: </source>
+        <translation>Exception non gérée :</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="86"/>
+        <source>switch to code mode, may not be used with -r</source>
+        <translation>passer en mode code, ne peut pas être utilisé avec -r</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="88"/>
+        <source>switch to script mode, may not be used with -o</source>
+        <translation>passer en mode script, ne peut pas être utiliser avec -o</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="90"/>
+        <source>show the program version</source>
+        <translation>afficher la version du programme</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="91"/>
+        <source>show this help text</source>
+        <translation>afficher ce texte d&apos;aide</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="104"/>
+        <source>usage: </source>
+        <translation>usage :</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="104"/>
+        <source>[parameters]</source>
+        <translation>[paramètres]</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="104"/>
+        <source>filename</source>
+        <translation>nom de fichier</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="105"/>
+        <source>Parameters are:</source>
+        <translation>Les paramètres sont :</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="136"/>
+        <source>Unknown execution mode, please specify -o (script) or -r (code)</source>
+        <translation>Mode d&apos;exécution inconnu, merci de spécifier -o (script) ou -r (code)</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="146"/>
+        <source>Unable to read input file</source>
+        <translation>Impossible de lire le fichier d&apos;entrée</translation>
+    </message>
+    <message>
+        <location filename="../scriptexecuter.cpp" line="49"/>
+        <source>Reading script file failed due to an internal error</source>
+        <translation>La lecture du script a échoué à cause d&apos;une erreur interne</translation>
+    </message>
+    <message>
+        <location filename="../scriptexecuter.cpp" line="56"/>
+        <source>Input script file has a bad script schema</source>
+        <translation>Le fichier d&apos;entrée a un schéma incorrect</translation>
+    </message>
+    <message>
+        <location filename="../scriptexecuter.cpp" line="63"/>
+        <source>Input script file is too recent</source>
+        <translation>Le fichier d&apos;entrée est trop récent</translation>
+    </message>
+    <message>
+        <location filename="../scriptexecuter.cpp" line="77"/>
+        <source>Start execution failed</source>
+        <translation>Impossible de démarrer l&apos;exécution</translation>
+    </message>
+    <message>
+        <location filename="../scriptexecuter.cpp" line="96"/>
+        <source>Execution error: </source>
+        <translation>Erreur d&apos;exécution :</translation>
+    </message>
+</context>
+</TS>
