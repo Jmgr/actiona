497072be2370aaf991999de94c833adf780a4aa1
niconil <laurent.combe@free.fr>
2013-03-02 21:21:23 +0100

    P04+ Action de lecture des variables d'environnement
    + nouvelle tentative pour que le commit soit le plus clean possible

--- actions/actionpackdata/actionpackdata.h
+++ actions/actionpackdata/actionpackdata.h
@@ -32,6 +32,7 @@
 #include "actions/writeclipboarddefinition.h"
 #include "actions/readbinaryfiledefinition.h"
 #include "actions/writebinaryfiledefinition.h"
+#include "actions/readenvironmentdefinition.h"
 #include "actions/copyfiledefinition.h"
 #include "actions/webdownloaddefinition.h"
 
@@ -72,6 +73,7 @@ public:
 		addActionDefinition(new Actions::WriteClipboardDefinition(this));
 		addActionDefinition(new Actions::ReadBinaryFileDefinition(this));
 		addActionDefinition(new Actions::WriteBinaryFileDefinition(this));
+		addActionDefinition(new Actions::ReadEnvironmentVariableDefinition(this));
 		addActionDefinition(new Actions::CopyFileDefinition(this));
 		addActionDefinition(new Actions::WebDownloadDefinition(this));
 	}
