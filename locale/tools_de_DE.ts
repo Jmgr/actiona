d74f2d6c346515c6d3219fb5935cddec322caa9d
Soul Inferno DE <gamingtiger@privatemail.com>
2014-11-22 20:16:16 +0100

    first file: locale\tools_de_DE.ts
    locale\tools_de_DE.ts successfully translated. More translated files
    will be done in the next days.

--- locale/tools_de_DE.ts
+++ locale/tools_de_DE.ts
@@ -1,33 +1,33 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de_DE">
+<TS version="2.0" language="de_DE">
 <context>
     <name>Tools::Updater</name>
     <message>
         <location filename="../tools/updater.cpp" line="187"/>
         <source>Connection to the server was refused.</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Verbindung zum Server wurde abgewiesen.</translation>
     </message>
     <message>
         <location filename="../tools/updater.cpp" line="190"/>
         <source>Unable to establish a connection to the server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Verbindung zum Server konnte nicht hergestellt werden.</translation>
     </message>
     <message>
         <location filename="../tools/updater.cpp" line="193"/>
         <location filename="../tools/updater.cpp" line="256"/>
         <source>Connection timeout.</source>
-        <translation type="unfinished"></translation>
+        <translation>Verbindungszeitlimit überschritten.</translation>
     </message>
     <message>
         <location filename="../tools/updater.cpp" line="198"/>
         <source>Serverside error.</source>
-        <translation type="unfinished"></translation>
+        <translation>Serverfehler.</translation>
     </message>
     <message>
         <location filename="../tools/updater.cpp" line="201"/>
         <source>Connection error.</source>
-        <translation type="unfinished"></translation>
+        <translation>Verbindungsfehler.</translation>
     </message>
 </context>
 </TS>
