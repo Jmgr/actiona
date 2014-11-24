336b908902d2c438d3a6ac814849da1616532f2c
Jmgr <jmgr@jmgr.info>
2014-11-24 23:12:42 +0100

    Locales - updated the Qt locales for fr_FR and de_DE

---
+++ locale/qt_de_DE.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+    <dependencies>
+        <dependency catalog="qtbase_de"/>
+        <dependency catalog="qtscript_de"/>
+        <dependency catalog="qtquick1_de"/>
+        <dependency catalog="qtmultimedia_de"/>
+        <dependency catalog="qtxmlpatterns_de"/>
+    </dependencies>
+</TS>
