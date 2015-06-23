b5dfcafc347127f9379c05113df1f669a2476eba
Jmgr <jmgr@jmgr.info>
2015-06-23 16:36:57 +0200

    Global - renamed Actionaz to Actiona in some files

--- actiontools/version.cpp
+++ actiontools/version.cpp
@@ -1,13 +1,13 @@
 /*
-	Actionaz
-	Copyright (C) 2008-2010 Jonathan Mercier-Ganady
+	Actiona
+	Copyright (C) 2008-2015 Jonathan Mercier-Ganady
 
-	Actionaz is free software: you can redistribute it and/or modify
+	Actiona is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	Actionaz is distributed in the hope that it will be useful,
+	Actiona is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 	GNU General Public License for more details.
