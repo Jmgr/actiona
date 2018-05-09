b5b07e4d2424990f5023e44472f9f0e2f8dee4f0
Jmgr <jmgr@jmgr.info>
2018-05-09 11:02:25 +0200

    Removed copyright actual year since it is useless and creates commit noise every year

--- actexecuter/main.cpp
+++ actexecuter/main.cpp
@@ -1,6 +1,6 @@
 /*
     Actiona
-	Copyright (C) 2005-2017 Jonathan Mercier-Ganady
+    Copyright (C) 2005 Jonathan Mercier-Ganady
 
     Actiona is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
