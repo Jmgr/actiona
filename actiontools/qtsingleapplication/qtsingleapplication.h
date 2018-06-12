186121952f5400cc053ccf2748c5a88f82e88cb0
Jmgr <jmgr@jmgr.info>
2018-06-12 20:42:07 +0200

    Fixed compilation under Windows

--- actiontools/qtsingleapplication/qtsingleapplication.h
+++ actiontools/qtsingleapplication/qtsingleapplication.h
@@ -44,28 +44,13 @@
 ** 
 ****************************************************************************/
 
+#include "actiontools_global.h"
 
 #include <QApplication>
 
 class QtLocalPeer;
 
-#if defined(Q_OS_WIN)
-#  if !defined(QT_QTSINGLEAPPLICATION_EXPORT) && !defined(QT_QTSINGLEAPPLICATION_IMPORT)
-#    define QT_QTSINGLEAPPLICATION_EXPORT
-#  elif defined(QT_QTSINGLEAPPLICATION_IMPORT)
-#    if defined(QT_QTSINGLEAPPLICATION_EXPORT)
-#      undef QT_QTSINGLEAPPLICATION_EXPORT
-#    endif
-#    define QT_QTSINGLEAPPLICATION_EXPORT __declspec(dllimport)
-#  elif defined(QT_QTSINGLEAPPLICATION_EXPORT)
-#    undef QT_QTSINGLEAPPLICATION_EXPORT
-#    define QT_QTSINGLEAPPLICATION_EXPORT __declspec(dllexport)
-#  endif
-#else
-#  define QT_QTSINGLEAPPLICATION_EXPORT
-#endif
-
-class QT_QTSINGLEAPPLICATION_EXPORT QtSingleApplication : public QApplication
+class ACTIONTOOLSSHARED_EXPORT QtSingleApplication : public QApplication
 {
     Q_OBJECT
 
