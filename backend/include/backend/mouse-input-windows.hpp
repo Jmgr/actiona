f334b915e7463cccd4bae350daa956f1ee0b4f64
Jmgr <jmgr@jmgr.info>
2021-02-25 23:07:45 +0000

    Add backend WIP

---
+++ backend/include/backend/mouse-input-windows.hpp
@@ -0,0 +1,33 @@
+/*
+    Actiona
+    Copyright (C) 2005 Jonathan Mercier-Ganady
+
+    Actiona is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Actiona is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+    Contact: jmgr@jmgr.info
+*/
+
+#include "backend/mouse-input.hpp"
+
+namespace Backend
+{
+    class BACKENDSHARED_EXPORT MouseInputWindows : public MouseInput
+    {
+        Q_OBJECT
+
+    public:
+        bool isButtonPressed(Button button) const override;
+        QPoint cursorPosition() const override;
+    };
+}
---
+++ backend/translations/backend_fr_FR.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>Tools::Updater</name>
+    <message>
+        <location filename="../tools/updater.cpp" line="156"/>
+        <source>Connection to the server was refused.</source>
+        <translation>La connexion au serveur a échouée.</translation>
+    </message>
+    <message>
+        <location filename="../tools/updater.cpp" line="159"/>
+        <source>Unable to establish a connection to the server.</source>
+        <translation>Impossible d&apos;établir une connexion vers le serveur.</translation>
+    </message>
+    <message>
+        <location filename="../tools/updater.cpp" line="162"/>
+        <location filename="../tools/updater.cpp" line="229"/>
+        <source>Connection timeout.</source>
+        <translation>Connexion échouée.</translation>
+    </message>
+    <message>
+        <location filename="../tools/updater.cpp" line="167"/>
+        <source>Serverside error.</source>
+        <translation>Erreur coté serveur.</translation>
+    </message>
+    <message>
+        <location filename="../tools/updater.cpp" line="170"/>
+        <source>Connection error.</source>
+        <translation>Erreur de connexion.</translation>
+    </message>
+</context>
+<context>
+    <name>languagesName</name>
+    <message>
+        <location filename="../tools/languages.cpp" line="42"/>
+        <source>System language (if available)</source>
+        <translation>Langue du système (si disponible)</translation>
+    </message>
+    <message>
+        <location filename="../tools/languages.cpp" line="43"/>
+        <source>English (US)</source>
+        <translation>Anglais (États-Unis)</translation>
+    </message>
+    <message>
+        <location filename="../tools/languages.cpp" line="44"/>
+        <source>French (France)</source>
+        <translation>Français (France)</translation>
+    </message>
+</context>
+</TS>
